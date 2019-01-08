e749409c70e76239e1a25ade829a590b4e05aa7f
Ryan Walkden <rc0035768@student.riversidecollege.ac.uk>
2019-01-08 10:44:47 +0000

    ive finished my series resistor code

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -12,6 +12,8 @@ namespace Assignment_2
 {
     public partial class Form1 : Form
     {
+        private object lblAnswer;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +22,41 @@ namespace Assignment_2
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double R1 = Convert.ToDouble(textBox1.Text);
+                double R2 = Convert.ToDouble(textBox2.Text);
+                double R3 = Convert.ToDouble(textBox3.Text);
+                double R4 = Convert.ToDouble(textBox4.Text);
+
+                double ans = R1 + R2 + R3 + R4;
+
+                label4.Text = "answer =" + ans.ToString("0.00");
+
+            }
+            catch
+            {
+                label4.Text = "answer =";
+            }
         }
     }
 }
